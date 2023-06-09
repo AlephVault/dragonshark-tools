2a9be005853829afcb51ff46e852b2efbf21a673
Luis Masuelli <luis@datagran.io>
2023-06-08 20:44:48 -0500

    Adding the run_native function's contents.

---
+++ dslauncher/run_native.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import threading
+from typing import Callable
+from .hotkeys import kill_on_hotkey
+from .saves import load_dragonshark_save, store_dragonshark_save
+
+
+def run_game(directory: str, command: str, package: str, app: str, on_end: Callable[[], None]):
+    """
+    Executes a native game.
+    :param directory: Where is the game stored.
+    :param command: The command, inside the game.
+    :param package: The game's package.
+    :param app: The game's app.
+    :param on_end: What happens when the game is completely terminated and cleaned up.
+    """
+
+    # The steps for this one are the following:
+    # 1. Load the current game's saves.
+    load_dragonshark_save(package, app)
+
+    # 2. Run the game.
+    process = subprocess.Popen(["sudo", "-u", "gamer", os.path.join(directory, command)])
+
+    def _func():
+        process.wait()
+        store_dragonshark_save(package, app)
+        on_end()
+    threading.Thread(target=_func).start()
+
+    # 3. Wait until the game process ends:
+    # Also install a signal to kill it on hotkey Start + Select (hold both 3 seconds).
+    kill_on_hotkey(process)
