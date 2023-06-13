5fd23605c93c5f87c1ef704e73b828a7bb83c26c
Luis Masuelli <luis@datagran.io>
2023-06-12 22:24:12 -0500

    Adding two headers for Godot to work.

--- launcher/static_server.py
+++ launcher/static_server.py
@@ -13,6 +13,8 @@ class GzipHTTPRequestHandler(SimpleHTTPRequestHandler):
     """
 
     def end_headers(self):
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
         super().end_headers()
 
